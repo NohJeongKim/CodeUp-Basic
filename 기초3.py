cd8282715deb4175b77bffe04fee54f8f7802e7c
KIM NOH JEONG <97859517+NohJeongKim@users.noreply.github.com>
2022-01-21 08:29:47 +0900

    Update 기초3.py

--- 기초3.py
+++ 기초3.py
@@ -46,3 +46,67 @@ if n%2!=0: # 1,3,5,7을
     print('oh my god')
 else:
     print('enjoy')
+
+# 1161
+a,b=map(int, input().split())
+if a%2==0:
+    if b%2==0:
+        print('짝수+짝수=짝수')
+    else:
+        print('짝수+홀수=홀수')
+else:
+    if b%2==0:
+        print('홀수+짝수=홀수')
+    else:
+        print('홀수+홀수=짝수')
+
+# 1162
+y,m,d=map(int, input().split())
+standard=(y-m+d)
+if standard%10==0:
+    print('대박')
+else:
+    print('그럭저럭')
+
+# 1163
+from sys import stdin
+y,m,d=map(int, stdin.readline().split())
+standard=((y+m+d)%1000)//100
+if standard%2==0:
+    print('대박')
+else:
+    print('그럭저럭')
+
+# 1164
+from sys import stdin
+a,b,c=map(int, stdin.readline().split())
+if (a>170)&(b>170)&(c>170):
+    print('PASS')
+else:
+    print('CRASH')
+    
+# 1165
+from sys import stdin
+time, score=map(int, stdin.readline().split())
+
+count=0
+while time<90:
+    time+=5
+    count+=1
+    
+print(count+score)
+
+# 1166
+year=int(input())
+if (((year%4==0)&(year%100!=0))|(year%400==0)):
+    print("yes")
+else:
+    print("no")
+
+# 1167
+A1.
+from sys import stdin
+a,b,c=map(int, stdin.readline().split())
+list1=[a,b,c]
+list1=sorted(list1)
+print(list1[1])
