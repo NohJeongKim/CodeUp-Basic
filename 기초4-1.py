7200a90fe6d23b332371ed886c1ce75cbfe94f0d
KIM NOH JEONG <97859517+NohJeongKim@users.noreply.github.com>
2022-02-08 12:46:39 +0900

    Update 기초4-1.py

--- 기초4-1.py
+++ 기초4-1.py
@@ -235,3 +235,91 @@ for ii in range(1, n+1):
     factorial_count*=ii
     
 print(factorial_count)
+
+# 1277
+from sys import stdin
+N=int(stdin.readline())
+even_num=list(map(int, stdin.readline().split()))
+print(even_num[0], even_num[N//2], even_num[-1])
+
+# 1278
+from sys import stdin
+n=stdin.readline().rstrip() # 문자열을 그대로 넣어주고, 맨 끝 \n은 제거시켜준다.
+print(len(n))
+
+# 1279
+from sys import stdin
+a,b=map(int, stdin.readline().split())
+result=0
+
+for num in range(a, b+1):
+    if num%2==1:
+        result+=num
+    else:
+        result-=num
+
+print(result)
+
+# 1280
+from sys import stdin
+a,b=map(int, stdin.readline().split())
+result=0
+
+for num in range(a, b+1):
+    if num%2==0:
+        print('-'+str(num), end='') # 이 코드의 순서는 딱히 상관없음. 하지만, 줄 바꿈은 없어야 한다.
+        result-=num # 결과는 계속 계산시켜주기.
+    else:
+        print('+'+str(num), end='')
+        result+=num
+
+print('='+str(result)) # "=" 표시를 해줘야 하기 때문에, str로 바꾼 뒤 "+" 연산자를 이용하자!
+
+# 1281
+A1.
+from sys import stdin
+a,b=map(int, stdin.readline().split())
+result=0
+
+for num in range(a, b+1):
+    if (num==a)&(num%2==1):
+        print(str(num), end='')
+        result+=num
+    elif num%2==0:
+        print('-'+str(num), end='')
+        result-=num
+    elif num%2==1:
+        print('+'+str(num), end='')
+        result+=num
+        
+print('='+str(result))
+
+A2.
+from sys import stdin
+a,b=map(int, stdin.readline().split())
+result=0
+
+for num in range(a, b+1):
+    if (num==a)&(num%2==1):
+        result+=num
+        print(str(num), end='')
+    elif num%2==0:
+        result-=num
+        print('-'+str(num), end='')
+    elif num%2==1:
+        result+=num
+        print('+'+str(num), end='')
+        
+print('='+str(result))
+
+# 1282
+import math
+n = int(input())
+k = 0
+for i in range(1, n):
+    if math.sqrt(n - i) % 1 == 0:
+        k = i
+        n = int(math.sqrt(n - 1))
+        break
+
+print(k, n)
